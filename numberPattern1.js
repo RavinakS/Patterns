96094897b87d97b5da433706b2f9ff89a4ff17ea
RavinakS <ravinasingh697@gmail.com>
2025-06-30 23:01:49 +0530

    added the question too

--- numberPattern1.js
+++ numberPattern1.js
@@ -1,3 +1,7 @@
+//You are given a number n. You need to generate and print a pattern based on the given value of n.
+//For each row, starting from the first, print numbers in descending order from n down to 1.
+//Each number in a row is repeated as many times as the current row index (starting from n)
+
 const pattern = (n) => {
   for (let row = n; row >= 1; row--) {
     let rowLine = "";
